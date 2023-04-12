b81d7748f4e6644dde51c762c5fc72cd7d50c0d7
Aastha <aasthag.1406@gmail.com>
2023-04-12 23:22:44 +0530

    fix: image download from s3 host

--- components/imageGallery.js
+++ components/imageGallery.js
@@ -1,13 +1,27 @@
 import { TrashIcon, DownloadIcon } from "@heroicons/react/outline";
+import { saveAs } from 'file-saver'
 
 export default function ImageGallery({ images, deleteImage, isManager }) {
-  const downloadURI = (uri, name) => {
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = `${uri}`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const downloadURI = async (uri, name) => {
+    try {
+      const headers = new Headers();
+      headers.append('Content-Disposition', 'attachment');
+
+
+      const response = await fetch(uri, {
+        headers
+      });
+
+      if (!response.ok) {
+        throw new Error(response);
+      }
+
+      const blob = await response.blob();
+      saveAs(blob, name);
+
+    } catch (error) {
+      console.error('Failed to download file', error);
+    }
   };
 
   return (
